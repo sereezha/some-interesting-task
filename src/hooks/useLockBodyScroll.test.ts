5c8bf0f139554ee334d7c0d6e973cc46ad2e9301
Serhii Hovorovskyi <hovorovskyi@gmail.com>
2024-01-20 19:15:32 +0200

    add tests

--- src/hooks/useLockBodyScroll.test.ts
+++ src/hooks/useLockBodyScroll.test.ts
@@ -1,5 +1,18 @@
+import { renderHook } from '@testing-library/react';
+import { useLockBodyScroll } from './useLockBodyScroll';
+
 describe('useLockBodyScroll', () => {
-  it('should work', () => {
-    expect(2).toBe(1);
+  it('should remove scroll if `true` was passed', () => {
+    renderHook(() => useLockBodyScroll(true));
+
+    expect(document.documentElement.style.overflow).toEqual('hidden');
+  });
+
+  it('should restore scroll on unmount', () => {
+    const { unmount } = renderHook(() => useLockBodyScroll(true));
+
+    unmount();
+
+    expect(document.documentElement.style.overflow).not.toEqual('hidden');
   });
 });
--- src/hooks/useLockBodyScroll.ts
+++ src/hooks/useLockBodyScroll.ts
@@ -1,11 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-export function useLockBodyScroll(
-  initialLocked = false,
-  rootId = '__next'
-): [boolean, (locked: boolean) => void] {
-  const [locked, setLocked] = useState(initialLocked);
-
+export function useLockBodyScroll(locked = false) {
   useEffect(() => {
     if (!locked) {
       return () => {};
@@ -13,32 +8,11 @@ export function useLockBodyScroll(
 
     const target = document.documentElement;
     const originalOverflow = target.style.overflow;
-    const originalPaddingRight = target.style.paddingRight;
 
     target.style.overflow = 'hidden';
 
-    const root = document.getElementById(rootId);
-    const scrollBarWidth = root ? root.offsetWidth - root.scrollWidth : 0;
-
-    if (scrollBarWidth) {
-      target.style.paddingRight = `${scrollBarWidth}px`;
-    }
-
     return () => {
       target.style.overflow = originalOverflow;
-
-      if (scrollBarWidth) {
-        target.style.paddingRight = originalPaddingRight;
-      }
     };
-  }, [locked, rootId]);
-
-  useEffect(() => {
-    if (locked !== initialLocked) {
-      setLocked(initialLocked);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [initialLocked]);
-
-  return [locked, setLocked];
+  }, [locked]);
 }
